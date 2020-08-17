d8d67faf330089470208e456334b739a16e7c9bf
KART-HIKEYAREKULAPALLI <51162521+KART-HIKEYAREKULAPALLI@users.noreply.github.com>
2020-08-17 13:25:44 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,68 +1,34 @@
-#include <string>
-#include "opencv2/opencv.hpp"
-#include <iostream> 
+
+/** @file VedioStreamZMQ/main.cpp
+ *  @brief 
+ *         This code of ZMQ server
+ *   
+ *  
+ *  @author Rekulapalli Karthikeya
+ *  @bug No bugs
+ */
 
 #include <czmq.h>
-
 #include "zwssock.h" 
-  
-//using namspace cv;
+#include <math.h>
+#include <bits/stdc++.h> 
+#include "matbase.hpp"
 
-static std::string base64Encode(const unsigned char* Data, int DataByte)
+std::string stringcon(const char *s)
 {
-	 // code table
-	const char EncodeTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	 	//return value 
-	std::string strEncode;
-	unsigned char Tmp[4] = { 0 };
-	int LineLength = 0;
-	for (int i = 0; i < (int)(DataByte / 3); i++)
-	{
-		Tmp[1] = *Data++;
-		Tmp[2] = *Data++;
-		Tmp[3] = *Data++;
-		strEncode += EncodeTable[Tmp[1] >> 2];
-		strEncode += EncodeTable[((Tmp[1] << 4) | (Tmp[2] >> 4)) & 0x3F];
-		strEncode += EncodeTable[((Tmp[2] << 2) | (Tmp[3] >> 6)) & 0x3F];
-		strEncode += EncodeTable[Tmp[3] & 0x3F];
-		if (LineLength += 4, LineLength == 76) { strEncode += "\r\n"; LineLength = 0; }
-	}
-	 // Encode the remaining data
-	int Mod = DataByte % 3;
-	if (Mod == 1)
-	{
-		Tmp[1] = *Data++;
-		strEncode += EncodeTable[(Tmp[1] & 0xFC) >> 2];
-		strEncode += EncodeTable[((Tmp[1] & 0x03) << 4)];
-		strEncode += "==";
-	}
-	else if (Mod == 2)
-	{
-		Tmp[1] = *Data++;
-		Tmp[2] = *Data++;
-		strEncode += EncodeTable[(Tmp[1] & 0xFC) >> 2];
-		strEncode += EncodeTable[((Tmp[1] & 0x03) << 4) | ((Tmp[2] & 0xF0) >> 4)];
-		strEncode += EncodeTable[((Tmp[2] & 0x0F) << 2)];
-		strEncode += "=";
-	}
- 
- 
-	return strEncode;
+     std::string str(s);
+   
+     return str;
+
 }
-static std::string Mat2Base64(const cv::Mat &img)
+int strintoint(std::string x)
 {
-	 
-      
-       
-	std::string img_data;
-	std::vector<uchar> vecImg;
-	std::vector<int> vecCompression_params;
-	//vecCompression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
-	vecCompression_params.push_back(90);
-	cv::imencode(".png",img, vecImg, vecCompression_params);
-	img_data = base64Encode(vecImg.data(), vecImg.size());
-        
-	return img_data;
+std::istringstream iss (x);
+  int number;
+  iss >> number;
+  return (number/8192);
+
+
 }
 int main() 
 { 
@@ -83,8 +49,6 @@ int main()
        /*Bining Your socket to URL*/
 	zwssock_bind(sock, con);
 
-        /*Opencv Vedio cature*/
-        cv::VideoCapture cap(0);
 
        /*Message Pointer*/
         zmsg_t* msg;
@@ -93,45 +57,47 @@ int main()
        /*this is request ID from HTML */
         zframe_t *id;
 
+
+
 while(!zsys_interrupted){
+          
 
-           cv::Mat frame;
-           cap >> frame;
-           std::string base=Mat2Base64(frame);
-           char *cstr = new char[base.length() + 1];
-           strcpy(cstr, base.c_str());
 
-          msg = zwssock_recv(sock);
+        msg = zwssock_recv(sock);
+    	id = zmsg_pop(msg);
+        std::string string1(zmsg_popstr(msg));
+        int x=strintoint(string1)+1;
+        std::string recv="";
+        int i=0;
+        #pragma omp parallel num_threads(x)
+        {
+          
+          #pragma omp critical 
+          recv=recv+stringcon(zmsg_popstr(msg));
+          
+        }  
 
-		id = zmsg_pop(msg);
-
-		while (zmsg_size(msg))
-		{
-			char * str = zmsg_popstr(msg);
-
-			printf("%s\n", str);
-
-			free(str);
-		}
-
-		zmsg_destroy(&msg);
-
-		msg = zmsg_new();
-                
-		zmsg_push(msg, id);
-		zmsg_addstr(msg,cstr);
-                
-		int rc = zwssock_send(sock, &msg);
+        cv::Mat frame=Base2Mat(recv);
+        zmsg_destroy(&msg);
+        std::string base=Mat2Base64(frame);
+        char *cstr = new char[base.length() + 1];
+        strcpy(cstr, base.c_str());
+        msg = zmsg_new();
+        zmsg_push(msg, id);
+	    zmsg_addstr(msg,cstr);
+       
+	    int rc = zwssock_send(sock, &msg);
 		
-                 if (rc != 0)
-		{zmsg_destroy(&msg);}
+        if (rc != 0)
+        {zmsg_destroy(&msg);}
+          
+        delete [] cstr;
 
 
- }	    
-  
-  cap.release();
- 
-  cv::destroyAllWindows();
+      
+ }
+
+	    
 
   zwssock_destroy(&sock);
   
